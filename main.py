dc8cd2dbfcfb946e3cbc34a425916b6cebac2096
helzheng123 <helen.zheng@stonybrook.edu>
2023-08-08 23:31:32 +0000

    more examples with modules

---
+++ main.py
@@ -0,0 +1,13 @@
+# import module1
+# import module1 as mod1 
+# from module1 import *
+from module1 import addition as add, subtraction as sub
+
+output1 = add(5, 10)
+output2 = sub(5, 10)
+
+print('Output 1: ', output1)
+print('Output 2: ', output2)
+
+print(f"Output 1: {output1}")
+print(f'Output 2: {output2}')
