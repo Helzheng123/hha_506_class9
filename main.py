e56059fecd5807f08c8a061317fb565457c2389c
helzheng123 <helen.zheng@stonybrook.edu>
2023-08-08 23:35:18 +0000

    updated

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ output2 = sub(5, 10)
 print('Output 1: ', output1)
 print('Output 2: ', output2)
 
+# inserting the output within the quotes
 print(f"Output 1: {output1}")
 print(f'Output 2: {output2}')
