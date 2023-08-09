886d899f367e5f221640acebe387f94ecd968a7c
helzheng123 <helen.zheng@stonybrook.edu>
2023-08-09 00:31:38 +0000

    second update

---
+++ systemChecks.py
@@ -0,0 +1,11 @@
+import sys as mysystem
+
+print(mysystem.path)
+
+output = [
+    '/usr/lib/python39.zip', 
+    '/usr/lib/python3.9', 
+    '/usr/lib/python3.9/lib-dynload', 
+    '/usr/local/lib/python3.9/dist-packages', 
+    '/usr/lib/python3/dist-packages'
+]
